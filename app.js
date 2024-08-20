fc4d2329c853158947395fb86b439005018cbe09
setupndt <sarthakpandit802@gmail.com>
2024-08-20 15:27:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import ReactDOM from "react-router-dom";
+
+const parent=React.createElement("div",{id:"parent",},
+    React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am an  h1 tag"),React.createElement("h2",{},"I am an  h2 tag")]),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"I am an  h1 tag"),React.createElement("h2",{},"I am an  h2 tag")]));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
+root.render(parent);
